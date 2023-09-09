232adda60e0dadc5a89c4405712bc15b3468d232
SergeyCherevichenko <cherevichenkosn@gmail.com>
2023-09-09 18:00:02 +0500

    решение задач семинар 1 язык программирования Python

---
+++ main.py
@@ -0,0 +1,22 @@
+
+# a = "Я люблю Python очень очень!"
+# print(len(a)) 
+# print (a[:])  
+# print (a.lower())  
+# print(a.upper())
+# print (a.replace("Python", "Java"))
+# print (a[:3])
+# print(a[2:])
+# print (a[-1])
+# print (a[4:len(a):6])
+# print(a[:1] + a[2:7] + a[-6])
+
+
+
+
+
+
+
+
+
+
